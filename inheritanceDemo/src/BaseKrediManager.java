f362cde20ff8ecd395f494f154c5d3b85fa223df
Meltem Sert <meltemsert29@gmail.com>
2022-09-28 19:32:25 +0300

    homework2 done

---
+++ inheritanceDemo/src/BaseKrediManager.java
@@ -0,0 +1,7 @@
+public class BaseKrediManager {
+
+    //Bir class yalnızca bir tane class'ı extend edebilir.
+    public void Hesapla() {
+        System.out.println("Kredi hesaplandı.");
+    }
+}
