ee028529bfb45a241efd349d815f0c7bcd2ac0fd
Meltem Sert <meltemsert29@gmail.com>
2022-10-16 20:16:51 +0300

    3 rd week homework

---
+++ youtubeDemo/src/CarCreditManager.java
@@ -0,0 +1,13 @@
+public class CarCreditManager implements ICreditManager{
+
+    @Override
+    public void calculate() {
+        System.out.println("Araba kredisi hesaplandı.");
+    }
+
+    @Override
+    public void save() {
+        System.out.println("Kaydedildi.");
+
+    }
+}
